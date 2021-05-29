b1ec76fe45f9de873241fbeb7df87d6998fde58e
mancancode <achonwaechris@outlook.com>
2021-05-29 12:16:06 +0100

    updated project

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,9 +8,17 @@ const Navbar = () => {
     return (
         <div className="container mx-auto max-w-6xl">
             <nav className="flex justify-between  mt-16">
+                <div className="flex">
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+        </svg>
+            <span className="text-4xl font-bold mr-16">Nextspring</span>
             
-            <span className="text-4xl font-bold mr-16">Notion</span>
-            
+
+
+                </div>
+           
 
             <div>
             <ul className="flex space-x-2  flex justify-around">
